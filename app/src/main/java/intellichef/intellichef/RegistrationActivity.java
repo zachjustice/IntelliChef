5c70cb55766c75b2be398fa636c55fd542bfe913
btan7 <btan7@gatech.edu>
2017-01-22 21:37:49 -0500

    Registration controller

--- app/src/main/java/intellichef/intellichef/RegistrationActivity.java
+++ app/src/main/java/intellichef/intellichef/RegistrationActivity.java
@@ -1,14 +1,101 @@
 package intellichef.intellichef;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by b on 1/22/17.
+ */
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText mFirstNameView;
+    private EditText mLastNameView;
+    private EditText mEmailView;
+    private EditText mUsernameView;
+    private EditText mPassword;
+    private EditText mConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        mFirstNameView = (EditText) findViewById(R.id.firstName);
+        mLastNameView = (EditText) findViewById(R.id.lastName);
+        mEmailView = (EditText) findViewById(R.id.email);
+        mUsernameView = (EditText) findViewById(R.id.username);
+        mPassword = (EditText) findViewById(R.id.password);
+        mConfirmPassword = (EditText) findViewById(R.id.confirmPassword);
+    }
+
+    public void attemptRegistration() {
+        mFirstNameView.setError(null);
+        mLastNameView.setError(null);
+        mEmailView.setError(null);
+        mUsernameView.setError(null);
+        mPassword.setError(null);
+        mConfirmPassword.setError(null);
+
+        String firstName = mFirstNameView.getText().toString();
+        String lastName = mLastNameView.getText().toString();
+        String email = mEmailView.getText().toString();
+        String username = mUsernameView.getText().toString();
+        String password = mPassword.getText().toString();
+        String confirmPassword = mConfirmPassword.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(firstName)) {
+            mFirstNameView.setError("This field is required");
+            focusView = mFirstNameView;
+            cancel = true;
+        } else if (TextUtils.isEmpty(email)) {
+            mEmailView.setError("This field is required");
+            focusView = mEmailView;
+            cancel = true;
+        } else if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError("This field is required");
+            focusView = mUsernameView;
+            cancel = true;
+        } else if (TextUtils.isEmpty(password)) {
+            mPassword.setError("This field is required");
+            focusView = mPassword;
+            cancel = true;
+        } else if (TextUtils.isEmpty(confirmPassword)) {
+            mConfirmPassword.setError("This field is required");
+            focusView = mConfirmPassword;
+            cancel = true;
+        } else if (!isValidPassword()) {
+            mPassword.setError("Password not valid");
+            focusView = mPassword;
+            cancel = true;
+        } else if (!password.equals(confirmPassword)) {
+            mConfirmPassword.setError("Password confirmation does not"
+                    + " match password");
+            focusView = mConfirmPassword;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean isValidPassword() {
+        if (mPassword.getText().length() >= 6) {
+            return true;
+        }
+        return false;
     }
 }
