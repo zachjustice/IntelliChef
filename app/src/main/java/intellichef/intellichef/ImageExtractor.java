6f7ca54765bafeaaac6889939e4072818d103d05
Jatin Nanda <jatin.nanda1337@gmail.com>
2017-02-13 23:50:39 -0500

    Picasso compatability and ImageExtractor class for Meal Plan and calibration screen added

---
+++ app/src/main/java/intellichef/intellichef/ImageExtractor.java
@@ -0,0 +1,18 @@
+package intellichef.intellichef;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by jatin1 on 2/13/17.
+ */
+public class ImageExtractor {
+    //simple function to take a url and put it into an imageview
+    //Note: the context refers to the activity the ImageView is in, 'this', or getApplicationContext() should usually work
+    public static void loadIntoImage(Context context, String url, ImageView image) {
+        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(image);
+    }
+
+}
